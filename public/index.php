fcb9530513366d21b111e4e9d910cd470b1b0f8f
paxs <pedro.alexandre.soares@nttdata.com>
2022-11-05 19:28:24 +0000

    remove unused query string

--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@ $router->add('/classes', 'GET', ['controller' => 'Lesson', 'action' => 'list']);
 $router->add('/bookings', 'GET', ['controller' => 'Booking', 'action' => 'list']);
 
 $path = $_SERVER['PATH_INFO'] ?? '';
-$queryParams = $_SERVER['QUERY_STRING'] ?? '';
 $requestType = $_SERVER['REQUEST_METHOD'] ?? '';
 
 $url = [
