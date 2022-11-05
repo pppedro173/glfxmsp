877204762e610d0dc14cecaf033bf18cf1835b1e
paxs <pedro.alexandre.soares@nttdata.com>
2022-11-05 19:27:47 +0000

    composer autoload implemented

--- public/index.php
+++ public/index.php
@@ -1,12 +1,6 @@
 <?php
 
-spl_autoload_register(function($class) {
-    $root = dirname(__Dir__);
-    $file = $root  .'/'. str_replace('\\', '/', $class) . '.php';
-    if(is_readable($file)){
-        require $root . '/' . str_replace('\\', '/', $class). '.php';
-    }
-});
+require_once '../vendor/autoload.php';
 
 $router = new Core\Router();
 
