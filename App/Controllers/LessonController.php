5c95e444bf80fafb1fed6079d8f1374d648a73d7
paxs <pedro.alexandre.soares@nttdata.com>
2022-11-06 16:27:25 +0000

    initial logic, empty db rename controller messy names

--- App/Controllers/LessonController.php
+++ App/Controllers/LessonController.php
@@ -3,23 +3,30 @@
 namespace App\Controllers;
 
 use App\Models\Lesson;
-use App\Controllers\Controller;
+use DateTime;
 
-class LessonController extends Controller
+class LessonController extends BaseController
 {
     public function create(): void
     {
         try {
+            $startDate = new DateTime(date('Y-m-d', strtotime($this->requestData->startDate)));
+            $endDate = new DateTime(date('Y-m-d', strtotime($this->requestData->endDate)));
+            $lessonsArr = [];
             $lesson = [
-                "name" => $this->requestData->name,
-                "startDate" => $this->requestData->startDate,
-                "endDate" => $this->requestData->endDate,
-                "capacity" => $this->requestData->capacity
+                'name' => $this->requestData->name,
+                'capacity' => $this->requestData->capacity,
+                'date' => $startDate->format('Y-m-d')
             ];
 
-            Lesson::insert($lesson);
+            while($startDate->diff($endDate)->days >= 0){
+                Lesson::insert($lesson);
+                array_push($lessonsArr, $lesson);
+                $startDate->modify('+1 day');
+                $lesson['date'] = $startDate->format('Y-m-d');
+            }
 
-            $this->success($lesson, 200);
+            $this->success($lessonsArr, 200);
         } catch (\Exception $e) {
             $this->failure($e->getMessage(), $e->getCode());
         }
