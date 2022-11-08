52016fa135074b66e9dabc83b95779b50b72fb22
paxs <pedro.alexandre.soares@nttdata.com>
2022-11-08 10:04:58 +0000

    Remove custom error handler

--- Core/Error.php
+++ Core/Error.php
@@ -2,17 +2,8 @@
 
 namespace Core;
 
-use Exception;
-
 class Error
 {
-    public static function errorHandler(string $message, int $level, string $file, int $line): void
-    {
-        if(error_reporting() != 0){
-            throw new \ErrorException($message, 0, $level, $file, $line);
-        }
-    }
-
     public static function exceptionHandler($e): void
     {
         header('Content-Type: application/json; charset=utf-8', false, 500);
